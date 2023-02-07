aa7c0e913c84bc9b8578e2008474f28bb9b9d38c
lily <a0976198586@gmail.com>
2023-02-07 23:06:17 +0800

    Update 2023-02-07T15:06:15.631Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,9 @@
 
 var loginPage = document.querySelector('.login-page'); //登入畫面
 var signupPage = document.querySelector('.signup-page'); //註冊畫面
+var addTodosPage = document.querySelector('.addTodos-page'); //內頁畫面
+var todoList = document.querySelector('.card-list'); //有代辦事項顯示這個
+var empty = document.querySelector('.empty'); //沒有代辦事項顯示這個
 // let token = '';
 var todoData = []; //抓出陣列資料放這裡
 var apiUrl = "https://todoo.5xcamp.us";
@@ -16,4 +19,12 @@ var loginAlertTxt = document.querySelector('.login-alert-txt');
 var loginModal = document.querySelector('#login-modal');
 var callLoginModal = new bootstrap.Modal(loginModal, {});
 // 註冊頁 DOM------------------------------------
+
+// addTodos內頁----------------------------------
+
+//登入頁----- //登入btn click事件
+loginBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(123);
+});
 //# sourceMappingURL=all.js.map
